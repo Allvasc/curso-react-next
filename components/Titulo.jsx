94c76cf1af36257e0a936ebbb8d1a6c5e5be25e1
Allison Vasconcelos <ah.vasconcelos@gmail.com>
2022-04-13 00:44:56 -0300

    first commit

---
+++ components/Titulo.jsx
@@ -0,0 +1,10 @@
+export default function Titulo(info) {
+    
+    console.log(info);
+    return(
+        <>
+            <h1>Título</h1>
+            <h2>Subtitulo</h2>
+        </>
+    )
+}
---
+++ pages/basicos/propriedade-dos-elementos.jsx
@@ -0,0 +1,12 @@
+import Titulo from "../../components/Titulo"
+
+export default function usandoTitulo() {
+    return (
+        <>
+            <Titulo
+                principal="Pagina de cadastro"
+                secundario="Incluir, alterar e excluir coisas!"
+            />
+        </>
+    )
+}
