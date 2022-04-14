f19c24f34fcea8adfc9bed34cb750256243d0609
Allison Vasconcelos <ah.vasconcelos@gmail.com>
2022-04-13 22:05:25 -0300

    update

--- components/Titulo.jsx
+++ components/Titulo.jsx
@@ -1,10 +1,14 @@
-export default function Titulo(info) {
-    
-    console.log(info);
-    return(
+export default function Titulo(props) {
+//operador ternário substitui a necessidade de if else
+    return props.pequeno ? (
         <>
-            <h1>Título</h1>
-            <h2>Subtitulo</h2>
+            <p>{props.principal}</p>
+            <p>{props.secundario}</p>
+        </>
+    ) : (
+        <>
+            <h1>{props.principal}</h1>
+            <h2>{props.secundario}</h2>
         </>
     )
 }
--- pages/basicos/propriedade-dos-elementos.jsx
+++ pages/basicos/propriedade-dos-elementos.jsx
@@ -2,11 +2,23 @@ import Titulo from "../../components/Titulo"
 
 export default function usandoTitulo() {
     return (
-        <>
+        <div>
             <Titulo
                 principal="Pagina de cadastro"
                 secundario="Incluir, alterar e excluir coisas!"
             />
-        </>
+            <Titulo
+                principal="Pagina de login"
+                secundario="Informe seu email e senha"
+                //atribuindo valor true explicitamente à pequeno
+                pequeno={true}
+            />
+            <Titulo
+                principal="Pagina de login"
+                secundario="Informe seu email e senha"
+                //atribuindo valor true inplicitamente à pequeno
+                pequeno
+            />
+        </div>
     )
 }
